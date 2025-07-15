9eaa5c4b4587f46b651c1252235e3257401f9fe6
CleanCodeNinja01 <saleha.shujaat1@gmail.com>
2025-07-15 17:35:43 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex h-full min-h-screen justify-center items-center'>
+      <h1 className='text-5xl text-center'>
+        React Router App
+      </h1>
+    </div>
+  )
+}
+
+export default App
